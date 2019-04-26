ce23b42ae0e39b85548b15323c166549d65ea387
MChojnowski <49043145+MChojnowski@users.noreply.github.com>
2019-04-25 19:01:34 -0500

    Update gridSearch.R

--- R/gridSearch.R
+++ R/gridSearch.R
@@ -207,7 +207,7 @@ gridSearch <- function(data,ph,pl,sent,mean.sent=mean(sent),sd.sent=sd(sent),min
   }
   
   cord_sse <- mat.SSE
-  cord_sse[roots >= 1] <- Inf
+  cord_sse[mat.ROOTS >= 1] <- Inf
   cord_sse <- which(cord_sse == min(cord_sse), arr.ind = TRUE)
   
   return(list(gridM=gridM
